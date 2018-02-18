ee21b5972ff6e9db148b30a54882411f7fe65476
ryotaro <princess.lilo.2323@gmail.com>
2018-02-18 14:26:00 +0900

    Add files via upload

---
+++ Assets/Bar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () { //一回
+		
+	}
+	
+	// Update is called once per frame
+	void Update () { //繰り返し
+		//ここ↓から
+		transform.Rotate(0f,0f,-0.1f);
+		//ここ↑まで
+		//コマンド＋B＝エラーか、わかる！！
+	}
+}
