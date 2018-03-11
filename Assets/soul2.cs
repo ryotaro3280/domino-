d2f51048ffef51988bf336332d2f9b075848ec4a
ryotaro <princess.lilo.2323@gmail.com>
2018-03-11 11:51:13 +0900

    3.11
    原型の完成？

---
+++ Assets/soul2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soul2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			transform.position += transform.up * 0.24f;
+		}
+		if (Input.GetKey ("right")) {
+			transform.position += transform.right * 0.1f;
+		}
+		if (Input.GetKey ("left")) {
+			transform.position += transform.right * -0.1f;
+		}
+	}
+}
