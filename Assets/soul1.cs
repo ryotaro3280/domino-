2a7dd3094286f64df4648794d398d096200f0744
ryotaro <princess.lilo.2323@gmail.com>
2018-05-13 11:39:23 +0900

    5/13
    鎌の追加、鎌の動きの追加

--- Assets/soul1.cs
+++ Assets/soul1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class soul2 : MonoBehaviour {
+public class soul1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,13 +12,16 @@ public class soul2 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey ("up")) {
-			transform.position += transform.up * 0.45f;
+			transform.position += transform.up * 0.2f;
 		}
 		if (Input.GetKey ("right")) {
-			transform.position += transform.right * 0.1f;
+			transform.position += transform.right * 0.2f;
 		}
 		if (Input.GetKey ("left")) {
-			transform.position += transform.right * -0.1f;
+			transform.position += transform.right * -0.2f;
+		}
+		if (Input.GetKey ("down")) {
+			transform.position += transform.up * -0.2f;
 		}
 	}
 }
