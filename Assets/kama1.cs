fad8cc4d2e250a4b391c664b85db815fedaefe0c
ryotaro <princess.lilo.2323@gmail.com>
2018-07-14 11:31:27 +0900

    7/14
    鎌の回転の追加　失敗
    サイトのリンク
    http://tama-lab.net/2017/06/unity%E3%81%A7%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%82%92%E5%9B%9E%E8%BB%A2%E3%81%95%E3%81%9B%E3%82%8B%E6%96%B9%E6%B3%95%E3%81%BE%E3%81%A8%E3%82%81/
    
    http://megumisoft.hatenablog.com/entry/2015/08/29/210310
    
    https://www.f-sp.com/entry/2016/08/15/190636
    
    http://oreunity.blogspot.com/2013/06/httpgamesonytablet.html
    
    http://tech.pjin.jp/blog/2016/12/22/unity_column_1/

--- Assets/kama1.cs
+++ Assets/kama1.cs
@@ -6,12 +6,30 @@ public class kama1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		for (int i=0;i<120;i++) {
-			transform.Rotate (3f, 0f, 0f);
-		}
+		//for (int i=0;i<120;i++) {
+			//transform.Rotate (3f, 0f, 0f);
+		//}
 	}
-	
+	float minAngle = 0.0F;
+	float maxAngle = 90.0F;
 	// Update is called once per frame
-	void Update () {
+			void Update() {
+		float angle = Mathf.LerpAngle (minAngle, maxAngle, Time.time);
+		transform.eulerAngles = new Vector3 (angle, 0, 0);
+		Invoke ("kiru1", 1f);
 	}
+		void kiru1() {
+		float angle = Mathf.LerpAngle (minAngle, maxAngle, Time.time);
+		transform.eulerAngles = new Vector3 (angle,0,0);
+			Invoke ("kiru2", 1f);
+		}
+		void kiru2() {
+		float angle = Mathf.LerpAngle (minAngle, maxAngle, Time.time);
+		transform.eulerAngles = new Vector3 (angle,0,0);
+			Invoke ("kiru3", 1f);
+		}
+		void kiru3() {
+		float angle = Mathf.LerpAngle (minAngle, maxAngle, Time.time);
+		transform.eulerAngles = new Vector3 (angle,0,0);
+		}
 }
